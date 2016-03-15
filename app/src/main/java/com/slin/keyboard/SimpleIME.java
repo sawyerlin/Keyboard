d887acc1dd54a1c4d76cbef4fac4c48a06843c3f
sawyerlin <sawyer.lin@gmail.com>
2016-03-15 16:15:36 +0100

    init commit

---
+++ app/src/main/java/com/slin/keyboard/SimpleIME.java
@@ -0,0 +1,76 @@
+package com.slin.keyboard;
+
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+public class SimpleIME extends InputMethodService
+        implements KeyboardView.OnKeyboardActionListener {
+
+    private KeyboardView kv;
+    private Keyboard keyboard;
+    private boolean caps = false;
+
+    @Override
+    public View onCreateInputView() {
+        kv = (KeyboardView)getLayoutInflater().inflate(R.layout.keyboard, null);
+        keyboard = new Keyboard(this, R.xml.qwerty);
+        kv.setKeyboard(keyboard);
+        kv.setOnKeyboardActionListener(this);
+        return null;
+    }
+
+    @Override
+    public void onPress(int primaryCode) {
+
+    }
+
+    @Override
+    public void onRelease(int primaryCode) {
+
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+        InputConnection ic = getCurrentInputConnection();
+        switch (primaryCode) {
+            case Keyboard.KEYCODE_DELETE:
+                break;
+            case Keyboard.KEYCODE_SHIFT:
+                break;
+            case Keyboard.KEYCODE_DONE:
+                break;
+            default:
+                char code = (char)primaryCode;
+                ic.commitText(String.valueOf(code), 1);
+                break;
+        }
+    }
+
+    @Override
+    public void onText(CharSequence text) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+}
